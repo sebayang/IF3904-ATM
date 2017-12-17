9d4330281a185770c43823ba73de0e7ade8b5fb6
sebayang <ibrahim.sebayang@gmail.com>
2017-12-17 16:21:52 +0700

    JDBC Version

---
+++ atmRut/src/Model/Aplikasi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import Database.Database;
+
+/**
+ *
+ * @author umann
+ */
+import java.util.*;
+import Database.Database;
+import Controller.ControllerLogin;
+
+public class Aplikasi {
+
+    private Database data;
+
+    public Aplikasi() {
+        this.data = new Database();
+        data.connect();
+    }
+    
+    public Nasabah LoginNasabah(int norek, int pin) {
+        Nasabah p = data.getNasabah(norek, pin);
+        if (p != null) {
+
+            return p;
+        } else {
+
+            return null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Aplikasi a = new Aplikasi();
+        new ControllerLogin(a);
+    }
+}
